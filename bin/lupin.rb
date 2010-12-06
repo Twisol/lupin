ad22ced2223642fa802b9f167ea17cbd5b18e467
Twisol <twisolar@gmail.com>
2010-12-06 01:44:56 -0800

    Implemented a very, very basic REPL in bin/lupin.rb

--- bin/lupin.rb
+++ bin/lupin.rb
@@ -6,4 +6,16 @@ require "bundler"
 Bundler.setup
 require "lupin"
 
-# Eventually, a REPL will go here.
+# Extremely basic, stupid-simple REPL.
+loop do
+  print '> '
+  begin
+    expr = gets
+    break unless expr
+    puts "=> #{Lupin.eval(expr.chomp)}"
+  rescue => ex
+    puts ex.backtrace
+  end
+end
+
+puts ""
